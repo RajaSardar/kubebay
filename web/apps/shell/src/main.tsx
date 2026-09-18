fb14167d86853f3beffd086833ae3aa2bb41c153
rajasardar <rj.raj@outlook.com>
2026-09-18 20:07:32 +0530

    fix: add missing StrictMode import in main.tsx

--- web/apps/shell/src/main.tsx
+++ web/apps/shell/src/main.tsx
@@ -3,6 +3,7 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./lib/theme";
